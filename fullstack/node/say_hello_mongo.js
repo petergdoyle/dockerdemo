237c3dec3e6432aec2e5d68524095196a527cedc
petergdoyle <peter.g.doyle@gmail.com>
2015-09-07 17:32:26 -0400

    fixed the connection uri for the mongodb test code

--- fullstack/node/say_hello_mongo.js
+++ fullstack/node/say_hello_mongo.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://mongo001:27017/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
