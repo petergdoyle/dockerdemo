1eac4c5aa62a0ecfd281835fc3cf6af415a984f2
petergdoyle <peter.g.doyle@gmail.com>
2015-10-05 10:24:15 -0600

    fixed bug in verify.sh

--- verify.sh
+++ verify.sh
@@ -22,10 +22,3 @@ do
     echo -e "\e[1;31mcannot run $cmd. it doesn't look like it was installed successfully\e[0m"
   fi
 done
-
-
-cmd='docker --version'
-eval $cmd > /dev/null 2>&1
-if [ $? -eq 127 ]; then
-
-fi
